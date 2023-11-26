7d6a4743770cce45bc132fbcba294bca886a8385
liangpeili <liangpeili@foxmail.com>
2023-11-26 22:24:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# ipfs-express
+
+A NodeJS IPFS App. This app will let you upload a file to a IPFS node.
+It's built with nodejs 10.17.0 and IPSF 0.7.0 under WSL2 Ubuntu
+
+see https://docs.microsoft.com/en-us/windows/dev-environment/overview To setup your environment.
+
+## Install
+
+IPFS cli version 0.7.0 for your system
+https://docs.ipfs.io/install/command-line/
+
+Node 10.17.0
+
+- Clone this repo and install packages
+
+```npm install```
+
+- Start the node
+
+```node index```
+
+Will listen on port 3000 of localhost by default.
+
+http://127.0.0.1:3000
+
+Upload a file to IPFS with the form. Edit index.js and the views to customize your app.
+
+
+
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,64 @@
+import fs from 'fs';
+import express from 'express';
+import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
+import { addFileToIPFS, addJSONToIPFS } from './ipfs-uploader.js';
+import { mint } from './nft-minter.js';
+import dotenv from 'dotenv';
+dotenv.config("./.env");
+
+const app = express()
+
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(fileUpload())
+
+app.get('/', (req,res) => {
+    res.render('home')
+    console.log('This is the home webpage')
+})
+
+app.post('/upload', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({ message: 'No files were uploaded.' });
+    }
+
+    const file = req.files.file;
+    const fileName = file.name
+    const filePath = 'files/' + fileName
+
+    const title = req.body.title
+    const description = req.body.description
+
+    file.mv(filePath, async (err) => {
+        if (err) {
+            console.log('error: failed to download the file.')
+            return res.status(500).send(err)
+        }
+
+        const fileResult = await addFileToIPFS(filePath)
+        console.log('File added to IPFS:', fileResult.cid.toString());
+
+        const metadata = {
+            title,
+            description,
+            image: 'https://ipfs.io/ipfs' + fileResult.cid.toString()
+        }
+        const jsonResult = await addJSONToIPFS(metadata)
+        console.log('Metadata added to IPFS:', jsonResult.cid.toString());
+
+        mint(process.env.ADDRESS, 'https://ipfs.io/ipfs' + jsonResult.cid.toString())
+
+        res.json({ 
+            message: 'File uploaded successfully.', 
+            metadata
+        });
+    })
+});
+
+const HOST = process.env.HOST 
+const PORT = process.env.PORT
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
